f58006b533c62733744d726cf46603b2cea3d785
Adjagarail <yayaadjagarailsadio@gmail.com>
2019-06-18 17:28:20 +0000

    connexion api

--- src/Components/menu.jsx
+++ src/Components/menu.jsx
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 
 class Menu extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded:false,
+        }
+    }
+    componentDidMount(){
+        fetch('http://localhost:8000/api/competences.json')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded:true,
+                    items:json,
+                })
+            });
+    }
     render(){
+        var { isLoaded, items } = this.state;
+        if(!isLoaded){
+            console.log('Chargement en cours....')
+            return(
+                <div> Chargement en cours ....</div>
+            );
+        }
+        else{
+            console.log(items)
         return(
             <div>
-
+                Données chargées
+                <ul>
+                    {items.map(item =>(
+                        <li key={item.id}>
+                            {item.Titre} | <img src={item.Image} alt="" srcSet={item.Image} />
+                        </li>
+                    ))};
+                </ul>
             </div>
         );
+        }
     }
 }
 export default Menu;
